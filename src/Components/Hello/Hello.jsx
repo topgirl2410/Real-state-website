8d345adba0d6ede1f2faecb2885ae481a84fc243
topgirl2410 <maria.topgirl82@gmail.com>
2023-11-04 23:16:01 +0100

    first commit

---
+++ src/Components/Hello/Hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Hello.css'
+
+const Hello = () => {
+    return (
+        <div>Hello</div>
+    )
+}
+
+export default Hello
---
+++ src/Components/Sponsors/Sponsor.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Sponsor.css'
+
+const Sponsor = () => {
+    return (
+        <div>Sponsor</div>
+    )
+}
+
+export default Sponsor
